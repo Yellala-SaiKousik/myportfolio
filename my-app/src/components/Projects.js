8b7a819773d384c55aff33d69b1c068cebf89f44
Yellala-SaiKousik <yskoushik@icloud.com>
2025-10-15 12:15:59 -0400

    Phase-1

---
+++ my-app/src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <ul>
+        <li><a href="https://aboardbuddy.com/#/auth/login" target="_blank" rel="noopener noreferrer">Aboard Buddy</a></li>
+        <li><a href="http://ec2-54-87-255-125.compute-1.amazonaws.com/" target="_blank" rel="noopener noreferrer">Learn Tracker</a></li>
+        <li><a href="https://github.com/Yellala-SaiKousik" target="_blank" rel="noopener noreferrer">GitHub Repositories</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
